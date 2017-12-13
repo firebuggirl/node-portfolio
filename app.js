c5f1ddfbbb720717dd27831c739dec5c68dedb59
Juliette <julietteredheadgirl@yahoo.com>
2017-12-13 13:55:54 -0600

    ssl middleware not working

--- app.js
+++ app.js
@@ -19,19 +19,19 @@ const fs = require("fs");
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  };
-};
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     next();
+//   };
+// };
 
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+//app.use(forceSSL());
 
 //require('dotenv').load();
 //var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.min.js", "utf-8");
