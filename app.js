641eb296db03b2dfd33d1eb87c0f70aa1935c09d
Juliette <julietteredheadgirl@yahoo.com>
2017-12-13 13:53:10 -0600

    try forceSSL middleware

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ const errorHandlers = require('./handlers/errorHandlers');
 
 //const jsdom = require("jsdom");
 //var jsdom = require("jsdom/lib/old-api.js");
-var fs = require("fs");
+const fs = require("fs");
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+  };
+};
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
 //require('dotenv').load();
 //var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.min.js", "utf-8");
