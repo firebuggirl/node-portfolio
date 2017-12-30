c8a9840172a5420772b0755050f4db90252b1ca9
Juliette <julietteredheadgirl@yahoo.com>
2017-12-30 14:38:06 -0600

    test

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(session({
   cookie: {
     secure: true,
     httpOnly: true, //protect against cross-site scripting attacks
-    // domain: 'https://juliettetworsey.com',
+     domain: 'https://juliettet-node-portfolio.herokuapp.com/',
     // path: 'foo/bar',
     expires: expiryDate
   }
